8a50b7a700181de5003784f944eb1ea7106d0d3f
caiohenriquesa1@gmail.com <caiohenriquesa1@gmail.com>
2024-11-16 19:39:02 -0300

    cs-add item to non-empty cart feature

--- src/js/product.js
+++ src/js/product.js
@@ -11,9 +11,24 @@ rawProduct.init();
 
 function addProductToCart(product) {
   getLocalStorage("so-cart") === null
-    ? setLocalStorage("so-cart", [{ product }])
-    : setLocalStorage("so-cart", [...getLocalStorage("so-cart"), { product }]);
+    ? setLocalStorage("so-cart", [{ product, quantity: 1 }])
+    : addItemToNonEmptyCart(product);
 }
+
+// increase the quantity of the item in the cart to avoid duplicates
+function addItemToNonEmptyCart(product) {
+  let items = getLocalStorage("so-cart");
+
+  // check product is there
+  const indexItem = items.findIndex((item) => item.product.Id == product.Id);
+  // if not, add it to cart
+  if (indexItem == -1) setLocalStorage("so-cart", [...items, { product, quantity: 1 }]);
+  else { // if so, increase its quantity and leave rest of cart as is
+    items[indexItem].quantity += 1;
+    setLocalStorage("so-cart", [...items]);
+  }
+}
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
